5b9e308d4c01c1eb638fa445c55ee58c35d13992
Aman Agarwal <amanaamazing@gmail.com>
2016-11-12 17:15:06 -0800

    removed test code from main.cc

--- main.cc
+++ main.cc
@@ -9,47 +9,9 @@
  
 #include "Bruinbase.h"
 #include "SqlEngine.h"
-#include "BTreeNode.h"
-#include "RecordFile.h"
-#include <cstdio>
 
 int main()
 {
-  printf("%lu\n", sizeof(BTNodeKeyRecord));
-  printf("%lu\n", sizeof(BTLeafNodeBuffer));
-  printf("%lu\n", sizeof(BTNonLeafNodeBuffer));
-
-  BTLeafNode btLeafNode;
-  RecordId rid;
-  rid.pid = 2;
-  rid.sid = 3;
-  for (int i = 90; i > 0; i--) {
-    if (i != 55) {
-      btLeafNode.insert(i, rid);
-    }
-  }
-  int eid;
-
-  BTLeafNode sibling;
-  int siblingKey;
-  btLeafNode.insertAndSplit(55, rid, sibling, siblingKey);
-  btLeafNode.locate(80, eid);
-  int key;
-  RecordId ridrecv;
-  btLeafNode.readEntry(eid, key, ridrecv);
-
-  BTNonLeafNode root;
-  root.initializeRoot(-1, -1, 0);
-
-  for (int i = 300; i > 0; i--) {
-    if (i != 55) {
-      root.insert(i, i);
-    }
-  }
-
-  BTNonLeafNode siblingnl;
-  int mid;
-  root.insertAndSplit(55, 55, siblingnl, mid);
 
   // run the SQL engine taking user commands from standard input (console).
   SqlEngine::run(stdin);
